b35494740aeee1fe59b469e579efd49e67fa2161
ledan404 <alfa7.plast@gmail.com>
2023-04-01 02:37:25 +0300

    add lab2

---
+++ src/ua/lviv/iot/algo/part1/lab2/Clip.java
@@ -0,0 +1,27 @@
+package ua.lviv.iot.algo.part1.lab2;
+
+import lombok.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString(callSuper = true)
+
+public class Clip extends Video {
+    private String artist;
+
+    private int like;
+
+    private int viems;
+
+    public double getCurrentRating() {return like / viems;}
+
+
+
+    public Clip(String title, String director, Integer year, String artist, int like, int viems){
+        super(title, director, year);
+        this.viems = viems;
+        this.artist = artist;
+        this.like = like;
+    }
+}
---
+++ src/ua/lviv/iot/algo/part1/lab2/Video.java
@@ -0,0 +1,18 @@
+package ua.lviv.iot.algo.part1.lab2;
+
+import lombok.*;
+
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+abstract class Video {
+    private String title;
+    private String director;
+
+    private Integer year;
+
+    public double getCurrentRating(double rating) {
+        return rating;
+    }
+
+}
